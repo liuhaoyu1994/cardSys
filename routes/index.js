6d78c372a64717e1f715d4c22dbdd915a62b9f3b
liuhaoyu1994 <liuhaoyuca@gmail.com>
2019-10-11 05:15:26 -0700

    test wechat

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log(req)
-  res.status(200).json({Token: '2333',Req:req});
+  res.status(200).json({Token: '2333',Req:req.body});
 });
 
 module.exports = router;
